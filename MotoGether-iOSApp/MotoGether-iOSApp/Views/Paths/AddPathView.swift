8d90db77153504600f07dee35b73718e7812bc40
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2023-03-11 11:37:49 +0100

    Add AddPathView layout

--- MotoGether-iOSApp/MotoGether-iOSApp/Views/Paths/AddPathView.swift
+++ MotoGether-iOSApp/MotoGether-iOSApp/Views/Paths/AddPathView.swift
@@ -8,13 +8,221 @@
 import SwiftUI
 
 struct AddPathView: View {
+    @State var username = ""
+    
     var body: some View {
-        Text("Add Path Screen")
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Add new event")
+                        .font(.title2)
+                        .bold()
+                    
+                    Spacer()
+                }
+                .padding(.bottom, 30)
+                
+                Group {
+                    ContentGroupView1()
+                    
+                    Divider()
+                        .padding(.bottom, 10)
+                        .padding(.top, 10)
+                    
+                    ContentGroupView2()
+                    
+                    Divider()
+                        .padding(.bottom, 10)
+                        .padding(.top, 10)
+                    
+                    ContentGroupView3()
+                    
+                }
+            }
+            .padding(.leading, 20)
+            .padding(.trailing, 20)
+            .padding(.bottom, 100)
+            .background(Color.MainBackgroundColor)
+        }
+        .background(Color.MainBackgroundColor)
     }
 }
 
 struct AddPathView_Previews: PreviewProvider {
     static var previews: some View {
         AddPathView()
+    }
+}
+
+struct ContentGroupView1: View {
+    var body: some View {
+        ChooseLocationView(title: "Start location")
+        ChooseLocationView(title: "End location")
+        AddStopView()
+    }
+}
+
+struct ContentGroupView2: View {
+    var body: some View {
+        TextInputView(title: "Duration", placeholder: "70 min")
+        TextInputView(title: "Date", placeholder: "11.3.2023")
+            .padding(.top, 10)
+    }
+}
+
+struct ContentGroupView3: View {
+    @State var onlyForFriends = false
+    @State var limitedParticipants = false
+    
+    var body: some View {
+        CheckBoxView(description: "Only for friends", checked: $onlyForFriends)
+        CheckBoxView(description: "Limited participants", checked: $limitedParticipants)
+        
+        Divider()
+            .padding(.bottom, 10)
+            .padding(.top, 10)
+        
+        TextInputView(title: "Description")
+            .padding(.top, 10)
+        
+        Button(action: {}) {
+            Text("Create event")
+                .frame(maxWidth: .infinity)
+        }
+        .frame(height: 50)
+        .buttonStyle(.borderedProminent)
+        .foregroundColor(.white)
+        .fontWeight(.semibold)
+        .background(Color.PrimaryColor)
+        .cornerRadius(10)
+        .padding(.top, 30)
+    }
+}
+
+struct ChooseLocationView: View {
+    var title = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .font(.body)
+                    .bold()
+                
+                Spacer()
+            }
+            
+            Button(action: {}) {
+                HStack{
+                    Text("Choose location")
+                        .padding()
+                        .foregroundColor(Color.TextColorPrimary)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "map.fill")
+                        .padding(.trailing, 15)
+                        .foregroundColor(Color.TextColorPrimary)
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.TextColorPrimary, lineWidth: 1.5)
+                )
+            }
+            .background(.thinMaterial.opacity(0.5))
+            .cornerRadius(10)
+            .padding(.bottom, 10)
+        }
+    }
+}
+
+struct AddStopView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Add stop")
+                    .font(.body)
+                    .bold()
+                
+                Spacer()
+            }
+            
+            HStack {
+                Button(action: {}) {
+                    HStack{
+                        Text("New Stop")
+                            .padding()
+                            .foregroundColor(Color.TextColorPrimary)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "plus")
+                            .padding(.trailing, 15)
+                            .foregroundColor(Color.TextColorPrimary)
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.TextColorPrimary, lineWidth: 1.5)
+                    )
+                }
+                .frame(width: UIScreen.main.bounds.width / 2)
+                .background(.thinMaterial.opacity(0.5))
+                .cornerRadius(10)
+                .padding(.bottom, 10)
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct TextInputView: View {
+    var title = ""
+    var placeholder = ""
+    @State var username = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .font(.body)
+                    .bold()
+                
+                Spacer()
+            }
+            
+            TextField(placeholder, text: $username)
+                .padding()
+                .background(.thinMaterial.opacity(0.5))
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.TextColorPrimary, lineWidth: 1)
+                )
+        }
+    }
+}
+
+struct CheckBoxView: View {
+    var description: String
+    @Binding var checked: Bool
+
+    var body: some View {
+        HStack {
+            Image(systemName: checked ? "checkmark.square.fill" : "square")
+                .resizable()
+                .foregroundColor(checked ? Color.PrimaryColor : Color.TextColorSecondary)
+                .onTapGesture {
+                    self.checked.toggle()
+                }
+                .frame(width: 20, height: 20)
+            
+            Text(description)
+                .padding()
+                .font(.subheadline)
+                .foregroundColor(Color.TextColorPrimary)
+            
+            Spacer()
+        }
     }
 }
